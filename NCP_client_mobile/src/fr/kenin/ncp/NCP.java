a68373637fee7bd0a2d10c97ab8e35ad1346eabf
Kevin.poirier72 <Kevin.poirier72@gmail.com>
2011-01-14 01:36:55 +0000

    Mise en place de la base du client Mobile

---
+++ NCP_client_mobile/src/fr/kenin/ncp/NCP.java
@@ -0,0 +1,13 @@
+package fr.kenin.ncp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NCP extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
