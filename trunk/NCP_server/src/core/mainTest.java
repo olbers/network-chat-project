14d66224e8c58bfb9ea36cd8b2c0baa80ce903da
Kevin.poirier72@gmail.com <Kevin.poirier72@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-01-15 00:53:30 +0000

    Ajout de la class de gestion de fichier Option.java Ajout du fichier de configuration option.conf Ajout d'une classe Main de test mainTest.java Ajout de la première version de la javadoc

---
+++ trunk/NCP_server/src/core/mainTest.java
@@ -0,0 +1,16 @@
+package core;
+
+import java.io.IOException;
+
+public class mainTest {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Option optionTest = new Option();
+	}
+
+}
