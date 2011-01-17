f12caade85b9267674cb014c8a51cfd8e245df67
Kevin.poirier72 <Kevin.poirier72@gmail.com>
2011-01-17 22:11:44 +0000

    Modif du fichier option.conf Mise à jour du fichier mainTest.java

--- NCP_server/src/core/mainTest.java
+++ NCP_server/src/core/mainTest.java
@@ -13,20 +13,32 @@ public class mainTest {
 		// TODO Auto-generated method stub
 		String requete;
 		ArrayList user=null;
-		Option optionTest = new Option();
+		Log newLog =new Log();
+		Option optionTest = new Option(newLog);
+		newLog.chat("Blabla");
+		/*String err="Erreur de ";
+		newLog.err(err);
+		String chat="BlablaBla";
+		newLog.chat(chat);
 		MySQL BDD = new MySQL(optionTest);
 		requete = "INSERT INTO user VALUES (2,'Kenin')";
 		//BDD.updateSQL(requete);
-		requete = "Select * from user";
+		requete = "Select * from user ";
 		user = BDD.selecSQL(requete, "ID","pseudo");
+		System.out.println(user.size());
+		for (int i=0;i<(user.size());i++){
+			if (user.get(i)==""){
+				System.out.println("Client in trouvable");
+			}
+			System.out.println(user.get(i));
+		}
+		requete = "Select * from user  WHERE pseudo = 'test02'";
+		user = BDD.selecSQL(requete, "ID");
+		System.out.println(user.size());
 		for (int i=0;i<(user.size());i++){
 			System.out.println(user.get(i));
 		}
-		user = BDD.selecSQL(requete,"pseudo");
-		for (int i=0;i<(user.size());i++){
-			System.out.println(user.get(i));
-		}
-		
+		*/
 	}
 
 }
