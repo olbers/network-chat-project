5df34d78455741d1f46d58ae6981c9121e97f0ac
cedric.huchede@gmail.com <cedric.huchede@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-02-08 16:11:08 +0000

    Version graphique V0.1

--- trunk/NCP_client_lourd/src/core/Client.java
+++ trunk/NCP_client_lourd/src/core/Client.java
@@ -15,6 +15,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -24,6 +26,7 @@ import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
 import javax.swing.JTabbedPane;
+import javax.swing.JList;
 
 
 public class Client extends JFrame implements Runnable {
@@ -32,24 +35,26 @@ public class Client extends JFrame implements Runnable {
 	protected JPanel jContentPane = null;
 	protected JTextField saisieGeneral = null;
 	protected JButton boutonEnvoyerGeneral = null;
-	protected Socket priseClient;  
+	protected Socket socketClient;  //  @jve:decl-index=0:
 	protected PrintWriter versServeur;  
-	protected BufferedReader depuisServeur;
+	protected BufferedReader depuisServeur;  //  @jve:decl-index=0:
 	protected BufferedReader clavier;
 	protected String texteEnvoiGeneral=null; 
 	protected String texteConnexion=null;
-	protected String texteDeconnexion=null;  
-	protected String texteRecu=null; 
+	protected String commandeDeconnexion=null;  //  @jve:decl-index=0:
+	protected String texteRecu=null;  //  @jve:decl-index=0:
+	protected String texteMP=null;
 	protected JTextPane jTextPaneMP = null;  
 	protected JMenuBar menuBar = new JMenuBar();
 	protected JMenu menu1 = new JMenu("Fichier");
 	protected JMenu menu2 = new JMenu("Aide");
 	protected JMenuItem item1 = new JMenuItem("Connexion");
+	protected JMenuItem item4 = new JMenuItem("Enregistrement");
 	protected JMenuItem item2 = new JMenuItem("Quitter");
 	protected JMenuItem item3 = new JMenuItem("A propos...");
-	protected String serveurIP;
+	protected String adresseIP;  //  @jve:decl-index=0:
 	protected int port;
-	protected String pseudo=null;
+	protected String pseudo=null;  //  @jve:decl-index=0:
 	protected boolean erreurCo=false;
 	protected JTabbedPane jTabbedPane = null;
 	protected JPanel jPanelGeneral = null;
@@ -60,89 +65,88 @@ public class Client extends JFrame implements Runnable {
 	protected JScrollPane jScrollPane2 = null;
 	protected JTextPane jTextPane = null;
 	protected JScrollPane jScrollPane = null;
-	protected JTextPane listeClient = null;
 	protected Thread ecoute;
-	
-	
+	protected ArrayList messagesGeneral;
+	protected ArrayList messagesPrives;
+	protected ArrayList clientsConnectes;
+	private JList jListClients = null;
+
+
+
+
 	/* Constructeur */
-	public Client(String serveur, int port , String pseudo) {	
-		
-		JFrame fenetre = new JFrame();								// Nouvelle fenêtre déclarée.
-		this.setTitle("ClientChatV1.0");							// Définition du titre de la fenêtre
-		this.setSize(new Dimension(700, 500));						// Définit une taille de la fenêtre. (700 par 500).
-		this.setContentPane(getJContentPane());						// Panel de la fenêtre.
-		this.setLocationRelativeTo(null);							// On positionne la fenètre au centre de l'écran					
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// Le processus se termine correctement lorsque l'on ferme la fenêtre
-		this.setResizable(false);									// On rend la fenêtre non redimentionnable
-		
-		this.serveurIP=serveurIP;
-		this.port=port;
-		this.pseudo=pseudo;
-		
-		/* Création du menu */
-		
+	public Client() {	
+		super();
+		getJFrame();
+
+		if (this.connexion(adresseIP, port)){
+			clavier = new BufferedReader(new InputStreamReader(System.in));
+			Thread ecoute = new Thread(this);
+			ecoute.start();
+			this.connexion(adresseIP, port);
+		}
+		else{
+			erreurCo=true;
+			System.err.println("Connexion impossible au serveur: vérifiez les paramètres de connexion!");
+			erreurConnexion();
+		}
+	}
+
+
+	public void menu(){
 		this.menuBar.add(menu1);
 		this.menu1.add(item1);
 		item1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,KeyEvent.CTRL_DOWN_MASK));
 		item1.addActionListener (new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
-				new DonneesConnexion();
-				dispose();
+				CreateConnexion();
 			}		
 		});
+
+		this.menu1.add(item4);
+		item4.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E,KeyEvent.CTRL_DOWN_MASK));
+		item4.addActionListener (new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				new Register();
+			}		
+		});
+
 		this.menu1.add(item2);
 		item2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,KeyEvent.CTRL_DOWN_MASK));
 		item2.addActionListener (new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				deconnexion();
-				
 				System.exit(0);
 			}		
 		});
-		
+
 		this.menuBar.add(menu2);
 		this.menu2.add(item3);
 		item3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,KeyEvent.CTRL_DOWN_MASK));
 		item3.addActionListener (new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				String textAbout="Nom: ClientChat\n" +
-								 "Auteur: HUCHEDÉ Cédric\n" +
-								 "Version: 1.0\n" +
-								 "                                                     ------------------------------------------------------------- \n\n"+
-								 "Description:\n" +
-								 "\n" +
-								 "Ce programme est un client permettant de se connecter à un serveur.\nIl permet de communiquer par messagerie instantanée à d'autres clients connectés au même serveur.\n";
+				"Auteur: HUCHEDÉ Cédric\n" +
+				"Version: 1.0\n" +
+				"                                                     ------------------------------------------------------------- \n\n"+
+				"Description:\n" +
+				"\n" +
+				"Ce programme est un client permettant de se connecter à un serveur.\nIl permet de communiquer par messagerie instantanée à d'autres clients connectés au même serveur.\n";
 				JOptionPane.showMessageDialog(null,
 						textAbout,
 						"A Propos...",
 						JOptionPane.INFORMATION_MESSAGE);				
 			}		
 		});
-		
+
 		menu1.setMnemonic('F');
 		menu2.setMnemonic('A');
-		
-		this.setJMenuBar(menuBar);
-		this.setVisible(true);
-		
 
-		if (this.connexion(serveurIP, port)){
-			clavier = new BufferedReader(new InputStreamReader(System.in));
-			//new Thread(this).start();
-			Thread ecoute = new Thread(this);
-			ecoute.start();
-			this.connexion();
-			connexionOK();
-		}
-		else{
-			erreurCo=true;
-			System.err.println("Connexion impossible au serveur: vérifiez les paramètres de connexion!");
-			erreur();
-		}
-	}	
-	
+		jFrame.setJMenuBar(menuBar);
+	}
+
 	/* Définition des composantes graphiques */
-	
+
 	/**
 	 * This method initializes jFrame	
 	 * 	
@@ -151,13 +155,18 @@ public class Client extends JFrame implements Runnable {
 	protected JFrame getJFrame() {
 		if (jFrame == null) {
 			jFrame = new JFrame();
-			jFrame.setSize(new Dimension(700, 500));
-			jFrame.setTitle("ClientChatV1.0");
-			jFrame.setContentPane(getJContentPane());
+			jFrame.setTitle("ClientChatV1.0");								// Définition du titre de la fenêtre
+			jFrame.setSize(new Dimension(700, 500));						// Définit une taille de la fenêtre. (700 par 500).
+			jFrame.setContentPane(getJContentPane());						// Panel de la fenêtre.
+			jFrame.setLocationRelativeTo(null);								// On positionne la fenètre au centre de l'écran					
+			jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			// Le processus se termine correctement lorsque l'on ferme la fenêtre
+			jFrame.setResizable(false);										// On rend la fenêtre non redimentionnable
+			menu();
+			jFrame.setVisible(true);
 		}
 		return jFrame;
 	}
-	
+
 	/**
 	 * This method initializes jContentPane	
 	 * 	
@@ -171,7 +180,7 @@ public class Client extends JFrame implements Runnable {
 		}
 		return jContentPane;
 	}
-	
+
 	/**
 	 * This method initializes jTabbedPane	
 	 * 	
@@ -187,7 +196,7 @@ public class Client extends JFrame implements Runnable {
 		}
 		return jTabbedPane;
 	}
-	
+
 	/* Onglet général */
 
 	/**
@@ -203,13 +212,13 @@ public class Client extends JFrame implements Runnable {
 			jPanelGeneral.add(getSaisieGeneral(), null);
 			jPanelGeneral.add(getJScrollPane2(), null);
 			jPanelGeneral.add(getJScrollPane(), null);
-			
+
 		}
 		return jPanelGeneral;
 	}
 
 	/* Onglet Messages personnels */
-	
+
 	/**
 	 * This method initializes jPanel1	
 	 * 	
@@ -223,7 +232,7 @@ public class Client extends JFrame implements Runnable {
 			jPanelMP.add(getJScrollPaneMP(), null);
 			jPanelMP.add(getSaisieMP(), null);
 			jPanelMP.add(getBoutonEnvoyerMP(), null);
-			
+
 		}
 		return jPanelMP;
 	}
@@ -285,7 +294,7 @@ public class Client extends JFrame implements Runnable {
 					if(e.getKeyChar() == KeyEvent.VK_ENTER){
 						texteEnvoiGeneral= saisieGeneral.getText();
 						saisieGeneral.setText("");		// On vide le champ de texte.
-						envoieMessageGeneral(texteEnvoiGeneral);
+						envoiMessageGeneral(texteEnvoiGeneral);
 					}
 				}
 			});
@@ -305,11 +314,9 @@ public class Client extends JFrame implements Runnable {
 			boutonEnvoyerGeneral.setBounds(new Rectangle(545, 370, 95, 35));
 			boutonEnvoyerGeneral.addActionListener(new java.awt.event.ActionListener() {
 				public void actionPerformed(java.awt.event.ActionEvent e) {
-					//System.out.println(texteEnvoi);
 					texteEnvoiGeneral= saisieGeneral.getText();
 					saisieGeneral.setText("");		// On vide le champ de texte.
-					//echo();
-					envoieMessageGeneral(texteEnvoiGeneral);
+					envoiMessageGeneral(texteEnvoiGeneral);
 				}
 			});
 		}
@@ -328,86 +335,7 @@ public class Client extends JFrame implements Runnable {
 		}
 		return jTextPaneMP;
 	}
-	
-	/* Définitions des méthodes du client */
-	
-	public boolean connexion(String serveur, int port) {
-		System.out.println("Connexion au serveur");
-		try {
-			this.priseClient = new Socket(serveur, port);
-			this.versServeur = new PrintWriter(priseClient.getOutputStream());
-			this.depuisServeur = new BufferedReader(new InputStreamReader(priseClient.getInputStream()));
-			return true;
-		} catch (UnknownHostException e) {
-			System.out.println("Serveur " + serveur + ":" + port + " inconnu");
-		} catch (IOException e) {
-			System.out.println("Erreur d'I/O");
-			e.printStackTrace();
-		}
-		return false;
-	}
 
-	public void envoieMessageGeneral(String message) {
-		this.versServeur.println(pseudo+": "+message);
-		this.versServeur.flush();
-	}
-	
-	public void envoiEvennement(String message){
-		this.versServeur.println(message);
-		this.versServeur.flush();
-	}
-
-	public String receptionMessageGeneral() {
-		try {
-			texteRecu = this.depuisServeur.readLine();
-			jTextPane.setText(jTextPane.getText()+"\n"+texteRecu);
-		} catch (IOException e) {
-			System.err.println("Erreur de lecture");
-		}
-		return texteRecu;
-	}
-	
-	public void connexion(){
-		
-			texteConnexion="Le client "+pseudo+" vient de se connecter.";
-			envoiEvennement(texteConnexion);
-	}
-	
-	public void deconnexion(){
-		
-		if(ecoute != null){
-		texteDeconnexion="Le client "+pseudo+" vient de se déconnecter.";
-		envoiEvennement(texteDeconnexion);
-		ecoute.stop();
-		}
-	}
-	
-	public void erreur(){
-		String messageErreur=jTextPane.getText()+"\n"+"Connexion impossible au serveur: vérifiez les paramètres de connexion!";
-		
-		jTextPane.setText(messageErreur);
-		jTextPane.setBackground(new Color(253, 241, 230));
-	}
-	
-	public void connexionOK(){
-		jTextPane.setText(jTextPane.getText()+"\n"+"Vous êtes connecté au serveur!");
-	}
-	
-	public void run() {
-		// TODO Auto-generated method stub
-		while(true){
-			receptionMessageGeneral();
-			depuisServeur=null;
-			try {
-				depuisServeur = new BufferedReader(new InputStreamReader(priseClient.getInputStream()));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-	
 	/**
 	 * This method initializes jScrollPane2	
 	 * 	
@@ -448,30 +376,163 @@ public class Client extends JFrame implements Runnable {
 		if (jScrollPane == null) {
 			jScrollPane = new JScrollPane();
 			jScrollPane.setBounds(new Rectangle(530, 20, 130, 308));
-			jScrollPane.setViewportView(getListeClient());	
+			jScrollPane.setViewportView(getJListClients());
 		}
 		return jScrollPane;
 	}
 
 
-
-	/**
-	 * This method initializes listeClient	
-	 * 	
-	 * @return javax.swing.JTextPane	
-	 */
-	protected JTextPane getListeClient() {
-		if (listeClient == null) {
-			listeClient = new JTextPane();
-			listeClient.setBackground(new Color(253, 241, 230));
-		}
-		return listeClient;
+	public void CreateConnexion(){
+		new Connexion(this);
 	}
 
+	public boolean connexion(String serveurIP, int port) {
+		System.out.println("Connexion au serveur...");
+		try {
+			this.socketClient = new Socket(serveurIP, port);
+			this.versServeur = new PrintWriter(socketClient.getOutputStream());
+			this.depuisServeur = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+			return true;
+		} catch (UnknownHostException e) {
+			System.out.println("Serveur " + serveurIP + ":" + port + " inconnu");
+		} catch (IOException e) {
+			System.out.println("Erreur Input/Output");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public void envoiMessageGeneral(String message) {
+		this.versServeur.println(message);
+		this.versServeur.flush();
+	}
+
+	public void envoiEvennement(String message){
+		this.versServeur.println(message);
+		this.versServeur.flush();
+	}
+
+	public String typeMessage(String message){
+		if(message.substring(0,1).equalsIgnoreCase("%")){
+			message=supprChar(message);
+			messagePrive(message);
+		}
+		else if(message.substring(0,1).equalsIgnoreCase("*")){
+			message=supprChar(message);
+			messageGeneral(message);
+		}
+		else if(message.substring(0,1).equalsIgnoreCase("$")){
+			// LISTE CONNECTES
+		}
+		else if(message.substring(0,1).equalsIgnoreCase("&")){
+			if(message.equalsIgnoreCase("&verif")){
+				// Renvoyer HASH MD5 de l'application au serveur
+			}
+			/* Incorporer ici les nouvelles commandes */
+			else{
+				System.err.println("Commande non supportée");
+			}
+
+		}
+		else if(message.substring(0,1).equalsIgnoreCase("#")){
+			messageSystem(message);
+		}
+		else{
+			System.err.println("SYNTAX MESSAGE ERROR: The message sent by server is not allowed by the protocol.");
+		}
+		return message;
+	}
+
+	public void messagePrive(String message) {		// Revoir selon ARRAYLIST
+		texteMP=texteMP+message;
+		jTextPaneMP.setText(texteMP+message);
+	}
+	public void messageSystem(String message){	// Print message system en rouge.
+
+		jTextPane.setEnabled(false);
+		jTextPane.setForeground(Color.RED);
+		jTextPane.setText(jTextPane.getText()+"\n"+message);//jTextPane.getText()+"\n"+message
+	}
+
+
+	public String supprChar(String message){
+		String chaineModif="";
+		for(int i=0;i<message.length()-1;i++){
+			chaineModif=chaineModif+message.charAt(i+1);
+		}
+		return chaineModif;
+	}
+
+	public void messageGeneral(String message) {
+		jTextPane.setText(jTextPane.getText()+"\n"+message); // Modifier avec ARRAYLIST
+	}
+
+	public void deconnexion(){
+
+		if(ecoute != null){
+			commandeDeconnexion="&deconnexion";
+			envoiEvennement(commandeDeconnexion);
+
+			try {
+				ecoute.stop();
+				this.depuisServeur.close();
+				this.versServeur.close();
+				this.socketClient.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void erreurConnexion(){
+		String messageErreur=jTextPane.getText()+"\n"+"Connexion impossible au serveur: vérifiez les paramètres de connexion!";
+		jTextPane.setText(messageErreur);
+	}
+
+	public void parametresConnexion(String adresseIP,int port,String pseudo){
+		this.adresseIP=adresseIP;
+		this.port=port;
+		this.pseudo=pseudo;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			String message=null;
+			depuisServeur=null;
+			try {
+				if(this.depuisServeur.ready()){
+					message = this.depuisServeur.readLine();
+					if(message != null){
+						typeMessage(message);
+					}
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * This method initializes jListClients	
+	 * 	
+	 * @return javax.swing.JList	
+	 */
+	private JList getJListClients() {
+		if (jListClients == null) {
+			jListClients = new JList();
+			jListClients.setBackground(new Color(253, 241, 230));
+			//jListClients.setSelectedIndex(-1);
+		}
+		return jListClients;
+	}
 
 
 	public static void main(String[] args) {
 
-		new DonneesConnexion(); // Lancement de la fenêtre de connexion.
+		new Client(); // Lancement de la fenêtre de connexion.
 	}
 }
