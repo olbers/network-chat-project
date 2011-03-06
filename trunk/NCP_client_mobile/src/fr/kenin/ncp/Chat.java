090e2891af371b310748600a41011848168c100d
Kevin.poirier72@gmail.com <Kevin.poirier72@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-03-06 02:24:49 +0000

    Modification et ajout d'interface

---
+++ trunk/NCP_client_mobile/src/fr/kenin/ncp/Chat.java
@@ -0,0 +1,11 @@
+package fr.kenin.ncp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chat extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chat);
+	}
+}
--- trunk/NCP_client_mobile/src/fr/kenin/ncp/Connexion.java
+++ trunk/NCP_client_mobile/src/fr/kenin/ncp/Connexion.java
@@ -1,12 +1,31 @@
 package fr.kenin.ncp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Connexion extends Activity {
+	
+	protected Context context;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.context=this;
 		setContentView(R.layout.connexion);
+		
+		((Button)findViewById(R.id.button3)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(context,Chat.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
 }
