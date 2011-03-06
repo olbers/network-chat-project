48c04e194692269ff1fc3bed0ba181c019d61134
Kevin.poirier72 <Kevin.poirier72@gmail.com>
2011-03-06 00:48:25 +0000

    Mise en place des première interface de l'app Android

---
+++ NCP_client_mobile/src/fr/kenin/ncp/Connexion.java
@@ -0,0 +1,12 @@
+package fr.kenin.ncp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Connexion extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.connexion);
+	}
+	
+}
