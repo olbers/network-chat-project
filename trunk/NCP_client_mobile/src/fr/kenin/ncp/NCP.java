42fcaab61af00a4182dfe32eb4788a448ce12df1
Kevin.poirier72@gmail.com <Kevin.poirier72@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-03-06 00:48:25 +0000

    Mise en place des première interface de l'app Android

--- trunk/NCP_client_mobile/src/fr/kenin/ncp/NCP.java
+++ trunk/NCP_client_mobile/src/fr/kenin/ncp/NCP.java
@@ -1,13 +1,42 @@
 package fr.kenin.ncp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NCP extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	
+	protected Context context;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		this.context=this;
+		((Button)findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(context,Connexion.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		
+	}
+
+	
 }
