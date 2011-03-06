91b23d9439d6168d8a492569c1c186f22822c7ab
Kevin.poirier72 <Kevin.poirier72@gmail.com>
2011-03-06 06:01:59 +0000

    Ajout d'un scrollview sur le textview du chat. Ajout d'un exemple de test

--- NCP_client_mobile/src/fr/kenin/ncp/Chat.java
+++ NCP_client_mobile/src/fr/kenin/ncp/Chat.java
@@ -9,6 +9,14 @@ public class Chat extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chat);
-
+		TextView text = ((TextView)findViewById(R.id.chatTextView1));
+		String message = "Salut <br> <font color='red'>tu va<br>  bien mois <br> nickqs dqsdqs<br>  dqsd</font>sqdq sdqs f<br>  fffff fffffffffffff<br>  ffffffff<br> " +
+				"fdq<br>plop jhdqhd <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ijij <br>  ij<br>" +
+				"Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> " +
+				"Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> " +
+				"Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> " +
+				"Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> " +
+				"Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br> Salut <br>  ";
+		text.setText(Html.fromHtml(message));
 	}
 }
