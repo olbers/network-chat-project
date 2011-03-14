089741b65dd01fdb96dee57525599e81421f6294
Kevin.poirier72@gmail.com <Kevin.poirier72@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-03-14 12:14:29 +0000

    Ajout Service Client Ajout d'écran Ajout de système d'encodage de chaine MD5.

---
+++ trunk/NCP_client_mobile/src/fr/kenin/ncp/Enregistrement.java
@@ -0,0 +1,15 @@
+package fr.kenin.ncp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Enregistrement extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.enregistrement);
+	}
+
+}
