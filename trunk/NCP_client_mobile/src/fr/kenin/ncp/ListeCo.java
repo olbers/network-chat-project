464cc030940a6d12505d1e7c086b949d945d0f84
Kevin.poirier72@gmail.com <Kevin.poirier72@gmail.com@29bc9951-4814-88a8-bf70-7367224acfd5>
2011-03-15 00:32:31 +0000

    Gestion liste des connectées Fix finish() dans connexion Gestion de deconnexion

--- trunk/NCP_client_mobile/src/fr/kenin/ncp/ListeCo.java
+++ trunk/NCP_client_mobile/src/fr/kenin/ncp/ListeCo.java
@@ -1,11 +1,15 @@
 package fr.kenin.ncp;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,33 +17,96 @@ import android.widget.TextView;
 public class ListeCo extends Activity {
 
 	protected ListView lv;
+	protected TextView saveTV;
 	protected ArrayList<String> userCO;
+	protected Intent intentAssoClient;
+	private Client mClient;
+
+	private static final String TAG = "ListCo_NCP";
 	/**
 	 * Permet de mettre à jour la liste des connecté.
-	 * @param liste
+	 * 
 	 */
-	public String setUserCo(String liste){		
-		StringTokenizer st = new StringTokenizer(liste,"|");
-		String pseudo;
-		StringBuffer listePrete = new StringBuffer("");
-		while (st.hasMoreTokens()) {
-			pseudo = st.nextToken();
-			listePrete.append(pseudo+"\n");
-		}
-		return listePrete.toString();
-	}
-
-	private void upDateList(String message){
-		String liste=setUserCo(message);
+	private void upDateList(){
+		String liste="";		
+		liste=mClient.getUtilisateurList();
 		TextView tv = (TextView)findViewById(R.id.listeCoTextView2);
+		Log.d(TAG, "Liste des connecté :"+liste);
 		tv.setText(liste);
-		
+
 	}
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listeco);
-		upDateList("");		
+		intentAssoClient = new Intent(this, Client.class);
+		doBindService();		
 	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		doUnbindService();
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		initConnexion();
+
+	}
+
+	private boolean initConnexion(){
+		if(mClient != null){
+			upDateList();
+			return true;
+		}else{
+			return false;
+		}
+	}	
+
+	private void doBindService() {
+		bindService(intentAssoClient, mConnexion, Context.BIND_AUTO_CREATE);
+	}
+
+	private void doUnbindService() {
+		unbindService(mConnexion);
+	}
+
+	private ServiceConnection mConnexion = new ServiceConnection(){
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			mClient = ((Client.ClientBinder) service).getService();
+			initConnexion();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			mClient=null;
+		}
+
+	};
 
 }
